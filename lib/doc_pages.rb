b18203d256876cd81c83404a95e2a199a1e77f48
lbp <43428385+leopolicastro@users.noreply.github.com>
2023-06-14 19:10:31 -0400

    format

--- lib/doc_pages.rb
+++ lib/doc_pages.rb
@@ -2,5 +2,4 @@ require "doc_pages/version"
 require "doc_pages/engine"
 
 module DocPages
-  # Your code goes here...
 end
--- lib/doc_pages/engine.rb
+++ lib/doc_pages/engine.rb
@@ -5,7 +5,7 @@ module DocPages
   class Engine < ::Rails::Engine
     isolate_namespace DocPages
 
-      initializer "engine_name.assets.precompile" do |app|
+    initializer "engine_name.assets.precompile" do |app|
       # app.config.assets.precompile += %w[docs_manifest.js]
       app.config.assets.precompile += %w[config/doc_pages_manifest.js]
     end
